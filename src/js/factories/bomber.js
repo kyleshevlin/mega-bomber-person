687ca7f368f2ad4289a0fdb9a5436018537d7523
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-03-21 23:19:20 -0700

    update bomber factory to function declaration

--- src/js/factories/bomber.js
+++ src/js/factories/bomber.js
@@ -1,42 +1,34 @@
 import bombFactory from './bomb'
 
-const bomberFactory = entity => {
-  const state = {
+export default function bomberFactory(entity) {
+  return {
     bombs: {
       count: 1,
       max: 1
+    },
+
+    incrementBombMax() {
+      this.bombs.max++
+      this.bombs.count++
+    },
+
+    dropBomb() {
+      if (this.bombs.count > 0) {
+        const bomb = bombFactory(entity)
+        bomb.plant()
+        this.bombs.count--
+
+        console.log(
+          'A bomb as dropped at coordinates: ' +
+            `${entity.position.x}, ${entity.position.y}`
+        )
+      }
+    },
+
+    replenishBomb() {
+      if (this.bombs.count < this.bombs.max) {
+        this.bombs.count++
+      }
     }
   }
-
-  const incrementBombMax = () => {
-    state.bombs.max++
-    state.bombs.count++
-  }
-
-  const dropBomb = () => {
-    if (state.bombs.count > 0) {
-      const bomb = bombFactory(entity)
-      bomb.plant()
-      state.bombs.count--
-
-      console.log(
-        'A bomb as dropped at coordinates: ' +
-          `${entity.position.x}, ${entity.position.y}`
-      )
-    }
-  }
-
-  const replenishBomb = () => {
-    if (state.bombs.count < state.bombs.max) {
-      state.bombs.count++
-    }
-  }
-
-  return Object.assign(state, {
-    dropBomb,
-    incrementBombMax,
-    replenishBomb
-  })
 }
-
-export default bomberFactory
